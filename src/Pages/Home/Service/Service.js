b4339777dbf07a02a3b7ad38aec60228c326e8f3
ARIFULHAQUE1 <arifulhaquearif2326@gmail.com>
2022-04-15 02:54:39 +0600

    responsive with meadia queary

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,16 +3,13 @@ import './Service.css'
 const Service = ({ service }) => {
       const { name, price, description, img } = service
       return (
-            <div>
-                  <div className="photo">
-                  <img src={img} alt="" />
-                  </div>
-                  <div>
-                  <h2>Service name:{name}</h2>
-                  <h4>Price:{price}</h4>
-                  <p>Description:{description}</p>
-                  </div>
-            </div>
+          <div className='service'>                   
+             <img src={img} alt="" />
+             <h4>Price:{price}</h4>
+             <p>{description}</p>
+             <button> Book For:{name}</button>
+          
+          </div>
       );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
-
+import './Services.css'
 const Services = () => {
-      const [services,setServices]=useState ([])
-      useEffect(()=>{
+      const [services, setServices] = useState([])
+      useEffect(() => {
             fetch('data.json')
-            .then(res=>res.json())
-            .then(data=>setServices(data))
-      },[])
+                  .then(res => res.json())
+                  .then(data => setServices(data))
+      }, [])
       return (
             <div>
-                 {
-                       services.map(service=><Service key={service.id} service={service}></Service>)
-                 }
+                  <h2 style={{color:'blueviolet', textAlign:'center'}}>Our Services</h2>
+                  <div className='services-container'>
+                        {
+                              services.map(service => <Service key={service.id} service={service}></Service>)
+                        }
+                  </div>
             </div>
       );
 };
